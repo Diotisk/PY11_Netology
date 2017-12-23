4c687f129a9e066e972c059b61014f48e2332b17
Daria <eremina.daria@gmail.com>
2017-12-23 18:03:33 +0300

    Changed format of strings.

--- test.py
+++ test.py
@@ -8,12 +8,12 @@ class Animal:
 
     def feed(self):
         feed_amount = float(input("Enter amount of feed in kg"))
-        print("The animal is fed with", feed_amount, "kg")
+        print("The animal is fed with {} kg".format(feed_amount))
         return feed_amount
 
     def graze(self):
         graze_durance = float(input("Enter amount of graze hours"))
-        print("The animal has been grazed for", graze_durance, "hours")
+        print("The animal has been grazed for {} hours".format(graze_durance))
         return graze_durance
 
 class Livestock(Animal):
@@ -27,7 +27,7 @@ class Cow(Livestock):
 
     def cow_milk(self):
         cow_milk_amount = float(input("Enter amount of milk in litres"))
-        print("Got", cow_milk_amount, "litres of cow milk")
+        print("Got {} litres of cow milk".format(cow_milk_amount))
         return cow_milk_amount
 
 class Goat(Livestock):
@@ -35,7 +35,7 @@ class Goat(Livestock):
 
     def goat_milk(self):
         goat_milk_amount = float(input("Enter amount of milk in litres"))
-        print("Got", goat_milk_amount, "litres of goat milk")
+        print("Got {} litres of goat milk".format(goat_milk_amount))
         return goat_milk_amount
 
 class Sheep(Livestock):
@@ -43,12 +43,12 @@ class Sheep(Livestock):
 
     def sheep_milk(self):
         sheep_milk_amount = float(input("Enter amount of milk in litres"))
-        print("Got", sheep_milk_amount, "litres of sheep milk")
+        print("Got {} litres of sheep milk".format(sheep_milk_amount))
         return sheep_milk_amount
 
     def shear(self):
         wool_amount = float(input("Enter amount of wool in kg"))
-        print("Got", wool_amount, "kg of wool")
+        print("Got {} kg of wool".format(wool_amount))
         return wool_amount
 
 class Pig(Livestock):
@@ -65,7 +65,7 @@ class Duck(Poultry):
 
     def lay_eggs_duck(self):
         duck_egg_amount = int(input("Enter the amount of eggs"))
-        print(duck_egg_amount, "of eggs were laid.")
+        print("{} of eggs were laid.".format(duck_egg_amount))
         return duck_egg_amount
 
 class Chicken(Poultry):
@@ -73,7 +73,7 @@ class Chicken(Poultry):
 
     def lay_eggs_chicken(self):
         chicken_egg_amount = int(input("Enter the amount of eggs"))
-        print(chicken_egg_amount, "of eggs were laid.")
+        print("{} of eggs were laid.".format(chicken_egg_amount))
         return chicken_egg_amount
 
 class Goose(Poultry):
