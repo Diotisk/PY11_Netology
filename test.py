1711e691a894d8d7726c12c479d2223c7fe493ca
Daria <eremina.daria@gmail.com>
2017-12-24 00:57:40 +0300

    modified gitignore

--- test.py
+++ test.py
@@ -6,85 +6,102 @@ class Animal:
         self.weight = weight
         self.age = age
 
-    def feed(self):
+    @staticmethod
+    def feed():
         feed_amount = float(input("Enter amount of feed in kg"))
         print("The animal is fed with {} kg".format(feed_amount))
         return feed_amount
 
-    def graze(self):
+    @staticmethod
+    def graze():
         graze_durance = float(input("Enter amount of graze hours"))
         print("The animal has been grazed for {} hours".format(graze_durance))
         return graze_durance
 
-class Livestock(Animal):
 
+class Livestock(Animal):
     def __init__(self, weight, age, colour):
         self.colour = colour
         super().__init__(weight, age)
+
 
 class Cow(Livestock):
     colour = None
 
-    def cow_milk(self):
+    @staticmethod
+    def cow_milk():
         cow_milk_amount = float(input("Enter amount of milk in litres"))
         print("Got {} litres of cow milk".format(cow_milk_amount))
         return cow_milk_amount
 
+
 class Goat(Livestock):
     colour = None
 
-    def goat_milk(self):
+    @staticmethod
+    def goat_milk():
         goat_milk_amount = float(input("Enter amount of milk in litres"))
         print("Got {} litres of goat milk".format(goat_milk_amount))
         return goat_milk_amount
 
+
 class Sheep(Livestock):
     colour = None
 
-    def sheep_milk(self):
+    @staticmethod
+    def sheep_milk():
         sheep_milk_amount = float(input("Enter amount of milk in litres"))
         print("Got {} litres of sheep milk".format(sheep_milk_amount))
         return sheep_milk_amount
 
-    def shear(self):
+    @staticmethod
+    def shear():
         wool_amount = float(input("Enter amount of wool in kg"))
         print("Got {} kg of wool".format(wool_amount))
         return wool_amount
 
+
 class Pig(Livestock):
     colour = None
 
-class Poultry(Animal):
 
+class Poultry(Animal):
     def __init__(self, weight, age, colour):
         self.colour = colour
         super().__init__(weight, age)
 
+
 class Duck(Poultry):
     colour = None
 
-    def lay_eggs_duck(self):
+    @staticmethod
+    def lay_eggs_duck():
         duck_egg_amount = int(input("Enter the amount of eggs"))
         print("{} of eggs were laid.".format(duck_egg_amount))
         return duck_egg_amount
 
+
 class Chicken(Poultry):
     colour = None
 
-    def lay_eggs_chicken(self):
+    @staticmethod
+    def lay_eggs_chicken():
         chicken_egg_amount = int(input("Enter the amount of eggs"))
         print("{} of eggs were laid.".format(chicken_egg_amount))
         return chicken_egg_amount
 
+
 class Goose(Poultry):
     colour = None
 
-    def pluck(self):
+    @staticmethod
+    def pluck():
         feather_amount = float(input("Enter amount of feather in kg"))
         print("Got {} kg of goose feather".format(feather_amount))
         return feather_amount
 
-#Test
+
+# Test
 Goose_0 = Goose(9, 2, "grey")
 Goose_1 = Goose(7.5, 3, "white")
 Goose_2 = Goose(4.4, 3.6, "rose")
@@ -98,8 +115,10 @@ for i in Geese:
 
 print("You got {} goose feather.".format(sum(sum_feather)))
 
-#Test2
+# Test2
 Sheep_0 = Sheep(15, 4, "white")
 Sheep_1 = Sheep(12, 10, "grey")
 print(Sheep_0.shear())
 print(Sheep_1.feed())
+
+import requests
