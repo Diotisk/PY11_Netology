54bdb998fc693469880c515818c8673d6e06cf9f
Daria <eremina.daria@gmail.com>
2017-12-28 22:28:46 +0100

    Deleted extra strings

--- readfile_hw.py
+++ readfile_hw.py
@@ -1,24 +1,28 @@
 cook_book = dict()
 ingredient_dict = dict()
-ingredient_list = []
+ingredient_list = [ingredient_dict]
 
 def search_ingredient():
     for line in file:
         if len(line) > 1:
-            #print(line)
             f = line.split("|")
+            #print(f)
             ingredient_dict["ingredient_name"] = f[0]
-            ingredient_dict["quantity"] = int(f[1])
+            ingredient_dict["quantity"] = f[1]
             ingredient_dict["measure"] = f[2].strip()
-            print(ingredient_dict)
+            #print(ingredient_dict)
         else:
             break
+        ingredient_list.append(ingredient_dict)
+        #print(ingredient_list)
     return ingredient_dict
 
 with open("cook_book.txt") as file:
     for line in file:
-        dish = file.readline()
-        print(dish.strip())
+        dish = line
+        #print(dish.strip())
         person_count = int(file.readline())
-        print(person_count)
+        #print(person_count)
         search_ingredient()
+        cook_book[dish] = ingredient_list
+    print(cook_book)
