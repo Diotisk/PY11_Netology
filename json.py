1061ab33f42f8b60bc5e79945e06a944de7071db
Daria <eremina.daria@gmail.com>
2018-01-18 00:32:06 +0100

    Decomposed the code, added searching for .json files in the current working directory, added search for most common words in each file, added try except part

--- json.py
+++ json.py
@@ -2,56 +2,63 @@ import simplejson as json
 import chardet
 from pprint import pprint
 import collections
+import os
 
-news_list = []
+cwd = os.getcwd()
 
 
-#def compose_news_list():
+def search_file(current_dir, extension):
+    searched_files = []
+    for file in os.listdir(current_dir):
+        i = os.path.splitext(file)
+        if extension in i:
+            searched_files.append(file)
+    print("All {} files:".format(extension), searched_files)
+    return searched_files
 
-with open("newsafr.json") as newsafr:
-    text_newsafr = json.load(newsafr)
-    news_list.append(text_newsafr)
+json_files = search_file(os.getcwd(), ".json")
 
-with open("newscy.json", "rb") as newscy:
-    t_newscy = newscy.read()
-    decoded_newscy = chardet.detect(t_newscy)
-    print(decoded_newscy)
-    result_newscy = t_newscy.decode(decoded_newscy["encoding"])
-    # pprint(result_newscy)
-    text_newscy = json.loads(result_newscy)
-    # pprint(text_newscy)
-    news_list.append(text_newscy)
 
-with open("newsfr.json", "rb") as newsfr:
-    t_newsfr = newsfr.read()
-    decoded_newsfr = chardet.detect(t_newsfr)
-    result_newsfr = t_newsfr.decode(decoded_newsfr["encoding"])
-    text_newsfr = json.loads(result_newsfr)
-    # pprint(text_newsfr)
-    news_list.append(text_newsfr)
+def read_file(filename):
+    with open(filename) as file:
+        content = json.load(file)
+    # pprint(content)
+    return content
 
-with open("newsit.json", "rb") as newsit:
-    t_newsit = newsit.read()
-    decoded_newsit = chardet.detect(t_newsit)
-    result_newsit = t_newsit.decode(decoded_newsit["encoding"])
-    text_newsit = json.loads(result_newsit)
-    # pprint(text_newsit)
-    news_list.append(text_newsit)
 
-# pprint(news_list)
-all_news = str()
+def decode_json(filename):
+    with open(filename, "rb") as file:
+        content = file.read()
+        decoded_content = chardet.detect(content)
+        # pprint(decoded_content)
+        result_content = content.decode(decoded_content["encoding"])
+        # pprint(result_content)
+        text = json.loads(result_content)
+        # pprint(text)
+    return text
 
-for j in news_list:
-    for i in j["rss"]["channel"]["items"]:
-        news_text = i["description"]
+
+def most_common_words(content_type):
+    content = content_type
+    all_news = str()
+    for i in content["rss"]["channel"]["items"]:
+        news_text = str(i["description"])
         all_news += news_text
     # pprint(all_news)
-    # pprint(type(all_news))
+    words_list = all_news.split(" ")
+    frequency = collections.Counter()
+    for word in words_list:
+        if len(word) > 6:
+            frequency[word] += 1
+    pprint(frequency.most_common(10))
+    return
 
-words_list = all_news.split(" ")
-
-frequency = collections.Counter()
-for word in words_list:
-    if len(word) > 6:
-        frequency[word] += 1
-pprint(frequency.most_common(10))
+for q in json_files:
+    try:
+        content_utf8 = read_file(q)
+        print(q, "utf-8")
+        most_common_words(content_utf8)
+    except UnicodeDecodeError:
+        print(q, "Needed decoding")
+        decoded_text = decode_json(q)
+        most_common_words(decoded_text)
