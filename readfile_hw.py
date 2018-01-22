dd1b2857b3bd8aeeb023a4f9c74a1a56833b816e
Daria <eremina.daria@gmail.com>
2018-01-22 15:16:36 +0300

    Deleted form_ingridient_list() inside read_cook_book(); renamed f into ingredient_data

--- readfile_hw.py
+++ readfile_hw.py
@@ -6,49 +6,45 @@ def read_cook_book():
             # print("Название блюда: {}".format(dish))
             file.readline()
             # print("Число под вопросом: {}".format(some_count))
-
-            def form_ingridient_list():
-                ingr_list = []
-                for i in file:
-                    i = i.strip()
-                    if not i:
-                        break
-                    else:
-                        ingridient = dict()
-                        # print(line)
-                        f = i.split("|")
-                        ingridient["ingridient_name"] = f[0].strip()
-                        ingridient["quantity"] = int(f[1])
-                        ingridient["measure"] = f[2].strip()
-                        ingr_list.append(ingridient.copy())
-                return ingr_list
-
-            ingridient_list = form_ingridient_list()
-            cook_book[dish] = ingridient_list
+            ingr_list = []
+            for i in file:
+                i = i.strip()
+                if not i:
+                    break
+                else:
+                    ingredient = dict()
+                    # print(line)
+                    ingredient_data = i.split("|")
+                    ingredient["ingredient_name"] = ingredient_data[0].strip()
+                    ingredient["quantity"] = int(ingredient_data[1])
+                    ingredient["measure"] = ingredient_data[2].strip()
+                    ingr_list.append(ingredient.copy())
+            cook_book[dish] = ingr_list
     return cook_book
 
 
 def get_shop_list_by_dishes(dishes, person_count):
     cook_book = read_cook_book()
+    print(cook_book)
     shop_list = {}
     for dish in dishes:
         # print(dish)
-        for ingridient in cook_book[dish]:
-            # print(ingridient)
-            new_shop_list_item = dict(ingridient)
+        for ingredient in cook_book[dish]:
+            # print(ingredient)
+            new_shop_list_item = dict(ingredient)
             # print(new_shop_list_item)
 
             new_shop_list_item['quantity'] *= person_count
-            if new_shop_list_item['ingridient_name'] not in shop_list:
-                shop_list[new_shop_list_item['ingridient_name']] = new_shop_list_item
+            if new_shop_list_item['ingredient_name'] not in shop_list:
+                shop_list[new_shop_list_item['ingredient_name']] = new_shop_list_item
             else:
-                shop_list[new_shop_list_item['ingridient_name']]['quantity'] += new_shop_list_item['quantity']
+                shop_list[new_shop_list_item['ingredient_name']]['quantity'] += new_shop_list_item['quantity']
     return shop_list
 
 
 def print_shop_list(shop_list):
     for shop_list_item in shop_list.values():
-        print('{} {} {}'.format(shop_list_item['ingridient_name'], shop_list_item['quantity'],
+        print('{} {} {}'.format(shop_list_item['ingredient_name'], shop_list_item['quantity'],
                                 shop_list_item['measure']))
 
 
