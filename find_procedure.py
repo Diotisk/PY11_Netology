0acf9eb8de9556a056381fd859a4ac32ea878811
Daria <eremina.daria@gmail.com>
2018-01-29 21:26:05 +0100

    Changed paths to the cwd

--- find_procedure.py
+++ find_procedure.py
@@ -5,35 +5,33 @@ migrations = "Migrations"
 current_dir = os.path.dirname(os.path.abspath(__file__))
 print("Current working directory: ", current_dir)
 
-os.chdir(migrations)
-new_current_dir = os.getcwd()
+new_current_dir = os.path.abspath(migrations)
 print("New current working directory: ", new_current_dir)
-# print("All files in the new cwd: ", os.listdir(new_current_dir))
+print("All files in the new cwd: ", os.listdir(new_current_dir))
 
 
 def search_files_cwd(extension, cwd):
     searched_files = []
     for file in os.listdir(cwd):
         i = os.path.splitext(file)
-        if extension in i:
+        if extension == i[1]:
             searched_files.append(file)
-    # print("SQL files only: ", searched_files)
-    # print("Number of SQL files: ", len(searched_files))
+    print("SQL files only: ", searched_files)
+    print("Number of SQL files: ", len(searched_files))
     return searched_files
-
-sql_files = search_files_cwd(".sql", new_current_dir)
 
 
 def search_files(files_list):
     count = 0
-    f = input("Search for: ")
+    searched_item = input("Search for: ")
     new_list = []
-    for j in files_list:
-        with open(j, "r") as content:
+    for file in files_list:
+        print(file, len(files_list))
+        with open(file, "r") as content:
             content_list = content.read()
-            if f in content_list:
-                # print(j)
-                new_list.append(j)
+            print(content_list)
+            if searched_item in content_list:
+                new_list.append(file)
                 count += 1
     for k in new_list:
         print(k)
@@ -41,9 +39,7 @@ def search_files(files_list):
     search_files(new_list)
 
 
-search_files(sql_files)
-
-
-# if __name__ == '__main__':
-#
-#     pass
+if __name__ == '__main__':
+    sql_files = search_files_cwd(".sql", new_current_dir)
+    search_files(sql_files)
+    pass
